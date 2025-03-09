e8d21b16e4760f2b4e82b94eb94c6b3edf5378f4
natanxavier <natanmiguel107@gmail.com>
2025-03-09 11:26:54 -0300

    operadores commit

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,24 @@
+public class Operadores {
+ 
+    public static void main(String[] args) {
+        
+    int a,b;
+    a = 6;
+    b = 6;
+
+    String resultado = a==b ?"verdadeiro" : "falso";
+
+    System.out.println(resultado);
+
+
+    String nomeUm = "GERSON";
+    String nomeDois = new String("GERSON");
+
+    System.out.println(nomeUm.equals(nomeDois));
+
+    // && == E
+    // || == OU
+
+}
+
+}
