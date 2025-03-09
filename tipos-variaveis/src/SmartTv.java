d4a138b0ed455183fd8970ad261dd849641b75d5
natanxavier <natanmiguel107@gmail.com>
2025-03-09 11:52:51 -0300

    metódos commit

---
+++ tipos-variaveis/src/SmartTv.java
@@ -0,0 +1,36 @@
+
+import javax.swing.JOptionPane;
+
+
+public class SmartTv { 
+        boolean ligada = false;
+        int canal = 1;
+        int volume = 25;
+
+        public void ligar(){
+            ligada = true;
+        }
+
+        public void desligar(){
+            ligada = false;
+        }
+
+        public void aumentarVolume(){
+            volume++;
+        }
+        public void abaixarVolume(){
+            volume--;
+        }
+
+        public void trocarCanal(){
+            canal++;
+        }
+        public void voltarCanal(){
+            canal--;
+        }
+        public void selecionarCanal(int novoCanal){
+           canal = novoCanal;
+        }
+
+
+}
---
+++ tipos-variaveis/src/Usuario.java
@@ -0,0 +1,31 @@
+public class Usuario {
+
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual : " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+
+        smartTv.ligar();
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+
+
+
+        smartTv.abaixarVolume();
+        smartTv.abaixarVolume();
+        smartTv.abaixarVolume();
+        smartTv.abaixarVolume();
+        smartTv.abaixarVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+
+    }
+
+}
