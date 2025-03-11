eebd77cac5cf532a948ce171e2cd73d55c669ee1
natanxavier <natanmiguel107@gmail.com>
2025-03-11 09:26:03 -0300

    condicao Ternaria commit

---
+++ tipos-variaveis/src/DOc.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Natan
+ * @version 1.0
+ * @since 09/03/2025
+ * 
+ */
+
+
+public class DOc {
+    /**
+     * @param parametroUm escreve o que é
+     * @param parametroDois escreve o que é
+     * @return diz o que retorna o metódo
+     */
+
+
+
+}
